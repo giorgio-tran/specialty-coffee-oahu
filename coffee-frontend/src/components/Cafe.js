f8df86c1092e9ca867a01af27c55509a81da2d7e
Giorgio <tran.giorgio@gmail.com>
2022-07-31 00:44:13 -1000

    Add components 'add' and 'cafe'

---
+++ coffee-frontend/src/components/Cafe.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Cafe = () => {
+    return
+}
+
+export default Cafe
--- coffee-frontend/src/components/Header.js
+++ coffee-frontend/src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Search from './Search'
 
 const Header = () => {
     return (
         <div>
             <div> Specialty Coffee Oahu </div>
+            <Search />
         </div>
     )
 }
