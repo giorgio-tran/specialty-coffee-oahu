a9bf522e14694670a33da4c06fa9e892f3e5a02d
Giorgio <tran.giorgio@gmail.com>
2022-07-31 17:52:48 -1000

    Add dummy data

--- coffee-frontend/src/App.js
+++ coffee-frontend/src/App.js
@@ -2,12 +2,26 @@ import Content from './components/Content'
 import Header from './components/Header'
 
 const App = () => {
+  const cafes = [
+    {
+      name: 'Alii Coffee',
+      website: 'https://www.aliicoffee.com/',
+      description: 'Great ambience, roast their own coffee.',
+      location: '',
+    },
+    {
+      name: 'The Curb',
+      website: 'https://thecurbkaimuki.com',
+      description: 'Good selection of coffees from various regions around the world',
+      location: '',
+    },
+    
+  ]
+
   return (
     <>
       <Header title="hello" />
-      <h1 className="text-3xl font-bold underline">
-        Hello World
-      </h1>
+      <Content />
     </>
   )
 }
