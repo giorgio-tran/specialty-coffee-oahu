e043ef9908d3df63e263ee6cc970bd2fd4ee8823
Giorgio <tran.giorgio@gmail.com>
2022-08-03 23:34:48 -1000

    Implement filter by cafe name

--- coffee-frontend/src/App.js
+++ coffee-frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Content from './components/Content'
 import Header from './components/Header'
 
@@ -8,16 +8,17 @@ const App = () => {
       name: 'Alii Coffee',
       website: 'https://www.aliicoffee.com/',
       description: 'Great ambience, roast their own coffee.',
-      location: '',
+      location: 'Kakaako',
     },
     {
       name: 'The Curb',
       website: 'https://thecurbkaimuki.com',
       description: 'Good selection of coffees from various regions around the world',
-      location: '',
+      location: 'Kaimuki',
     },
     
   ]
+
   //states
   const [newFilter, setNewFilter] = useState('')
   const [cafes, setCafes] = useState([])
@@ -25,8 +26,10 @@ const App = () => {
   const handleFilterChange = (event) => {
       setNewFilter(event.target.value)
   }
-  const handleCafes = (event) => {
+
+  const handleCafes = () => {
       setCafes(cafesList)
+      console.log(cafes)
   }
   
   return (
@@ -38,6 +41,7 @@ const App = () => {
       />
       <Content 
         content_cafes={cafesList}
+        content_filter={newFilter}
       />
     </>
   )
--- coffee-frontend/src/components/Content.js
+++ coffee-frontend/src/components/Content.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
-const Content = ({content_cafes}) => {
+const Content = ({content_cafes, content_filter}) => {
+    
+    const filterByCafeName = content_cafes.filter((cafe) => {
+            return (
+                cafe.name
+                    .toLowerCase()
+                    .includes(content_filter.toLowerCase())
+            )
+        }
+    
+    )
 
     return (
-        content_cafes.map((cafe) => {
+        filterByCafeName.map((cafe) => {
             return (
                 <button key={cafe.name}> 
                     {cafe.name}
