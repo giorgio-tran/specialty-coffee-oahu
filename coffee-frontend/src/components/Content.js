a6b42c3e8e93e88828d7547b0a03b587f54a5b3a
Giorgio <tran.giorgio@gmail.com>
2022-08-03 23:39:34 -1000

    Refactor filter in content

--- coffee-frontend/src/components/Content.js
+++ coffee-frontend/src/components/Content.js
@@ -1,18 +1,15 @@
 import React from 'react'
 
 const Content = ({content_cafes, content_filter}) => {
-    
-    const filterByCafeName = content_cafes.filter((cafe) => {
-            return (
-                cafe.name
-                    .toLowerCase()
-                    .includes(content_filter.toLowerCase())
-            )
-        }
-    
+    //filters based on name
+    const filterByCafeName = content_cafes.filter((cafe) => 
+        cafe.name
+            .toLowerCase()
+            .includes(content_filter.toLowerCase())
     )
 
     return (
+        //returns buttons
         filterByCafeName.map((cafe) => {
             return (
                 <button key={cafe.name}> 
