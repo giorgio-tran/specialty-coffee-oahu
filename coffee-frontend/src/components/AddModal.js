bbe09a71b67e2e2c010dee1af7db279c6352d49d
Giorgio <tran.giorgio@gmail.com>
2022-08-08 23:01:06 -1000

    Add features to AddModal

--- coffee-frontend/src/components/AddModal.js
+++ coffee-frontend/src/components/AddModal.js
@@ -10,9 +10,18 @@ const AddModal = ({ open, children, close_AddModal }) => {
         <>
             <div className='fixed inset-0 z-40 bg-black/[0.6]'></div>
             <div className='fixed flex justify-center items-center h-screen w-screen z-50 inset-0'>
-                <div className='box-border border-solid border-2 border-black bg-emerald-600'>
-                    <button className='border-solid border-2 border-black' onClick={close_AddModal}> x </button>
-                    {children}
+                <div className='box-border border-solid border-2 border-black bg-white h-1/2 w-1/2 py-0 px-2 flex flex-col justify-between'>
+                    {/* Header */}
+                    <div className='flex justify-end'>
+                        <button className='' onClick={close_AddModal}> x </button>
+                    </div>
+                    {/* Content */}
+                    <div>{children}</div>
+                    {/* footer */}
+                    <div className='flex justify-end gap-2 mb-2'>
+                        <button className='border-box border-2 border-black' onClick={close_AddModal}> cancel </button>
+                        <button className='border-box border-2 border-black'> add </button>
+                    </div>
                 </div>
             </div>
         </>,
