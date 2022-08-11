2054c6cbb75ca0f0d6b67eddac9e8106328eb912
Giorgio <tran.giorgio@gmail.com>
2022-08-11 09:09:47 -1000

    Refactor OnSubmit in AddModal

--- coffee-frontend/src/components/AddModal.js
+++ coffee-frontend/src/components/AddModal.js
@@ -15,7 +15,12 @@ const AddModal = ({ open, close, handleCafeSubmission }) => {
 		location: location,
 		description: description,
 	}
-
+	
+	const handleOnSubmit = event => {
+		event.preventDefault()
+		handleCafeSubmission(event, cafeObj)
+		event.target.reset()
+	}
 	const handleName = event => setName(event.target.value)
 	const handleWebsite = event => setWebsite(event.target.value)
 	const handleLocation = event => setLocation(event.target.value)
@@ -30,31 +35,27 @@ const AddModal = ({ open, close, handleCafeSubmission }) => {
     return ReactDOM.createPortal(
 		<>
             <div className='fixed inset-0 z-40 bg-black/[0.6]' />
-				<div className='fixed flex justify-center items-center h-screen w-screen z-50 inset-0'>
-					<div className='flex flex-col justify-between h-1/2 w-1/2 bg-white pt-0 px-4 rounded-md'>
-					{/* Header */}
-					<div className='flex justify-end'>
-						<button className='' onClick={close}> x </button>
-					</div>
-					{/* Content */}
-					<div>
-						{/* passes cafeObj to parent */}
-						<form onSubmit={event => {
-							event.preventDefault()
-							handleCafeSubmission(event, cafeObj)
-						}
-						}>
-							<InputForm name='name' onChange={handleName} />
-							<InputForm name='website' onChange={handleWebsite} />
-							<InputForm name='location' onChange={handleLocation} />
-							<InputForm name='description' onChange={handleDescription} />
-							<div className='flex justify-end gap-2 mb-2'>
-								<button className='border-box border-2 border-black' type='button' onClick={close}> cancel </button>
-								<button type='submit' className='border-box border-2 border-black'> add </button>
-							</div>
-						</form>
-					</div>
+			<div className='fixed flex justify-center items-center h-screen w-screen z-50 inset-0'>
+				<div className='flex flex-col justify-between h-1/2 w-1/2 bg-white pt-0 px-4 rounded-md'>
+				{/* Header */}
+				<div className='flex justify-end'>
+					<button className='' onClick={close}> x </button>
 				</div>
+				{/* Content */}
+				<div>
+					{/* passes cafeObj to parent */}
+					<form onSubmit={handleOnSubmit}>
+						<InputForm name='name' onChange={handleName} />
+						<InputForm name='website' onChange={handleWebsite} />
+						<InputForm name='location' onChange={handleLocation} />
+						<InputForm name='description' onChange={handleDescription} />
+						<div className='flex justify-end gap-2 mb-2'>
+							<button className='border-box border-2 border-black' type='button' onClick={close}> cancel </button>
+							<button type='submit' className='border-box border-2 border-black'> add </button>
+						</div>
+					</form>
+				</div>
+			</div>
             </div>
         </>,
         document.getElementById('portal')
