70ccfdcda8f0bc40fa0a357637cf3c564dfafc17
Giorgio <tran.giorgio@gmail.com>
2022-08-17 08:32:09 -1000

    Recycle app and index from fso p4

--- coffee-backend/index.js
+++ coffee-backend/index.js
@@ -1,23 +1,10 @@
+const app = require('./app')
+const http = require('http')
 const config = require('./utils/config')
-const mongoose = require('mongoose')
-const express = require('express')
-const app = express()
+const logger = require('./utils/logger')
 
-mongoose.connect(config.MONGODB_URI)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB')
-  })
+const server = http.createServer(app)
 
-  app.use(express.json())
-
-  app.get('/', (req, res) => {
-    res.send('<h1> hello world </h1>')
-  })
-
-  const PORT = config.PORT
-  app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`)
-  })
+server.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
+})
