80d3cd0a2922b5ca2281012e7cf2d4293da0b189
Giorgio <tran.giorgio@gmail.com>
2022-08-19 11:21:32 -1000

    Remove custom placeholder since TW already has it

--- coffee-frontend/src/components/Search.js
+++ coffee-frontend/src/components/Search.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Search = ({inputVal, onChangeFn}) => {
   return (
     <input
-      className='outline-none bg-beige box-border border-2 border-brown rounded-3xl w-80 px-5' 
+      className='outline-none bg-beige box-border border-2 border-brown rounded-3xl w-80 px-5 placeholder-darkbrown' 
       value={inputVal} 
       placeholder='Search...'
       onChange={onChangeFn}
