20912c0bf4a75143cb4863821c2ee2e29df5951f
Giorgio <tran.giorgio@gmail.com>
2022-08-19 12:00:31 -1000

    Add style to footer

--- coffee-frontend/src/components/Footer/Footer.js
+++ coffee-frontend/src/components/Footer/Footer.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='flex items-center p-10'> 
-      <a href='github.com/giorgio808'> 
+    <footer className='flex items-end justify-center pt-10 pb-4 text-darkbrown'> 
+      <a 
+        href='https://github.com/giorgio808' target='_blank' rel='noreferrer noopener'> 
         made by giorgio808
       </a>
     </footer>
