bb5d65710a9508285a33c467046f2fba6b72a383
Giorgio <tran.giorgio@gmail.com>
2022-08-19 16:06:27 -1000

    Remove h-full as it created an overflow bug

--- coffee-frontend/src/components/Content.js
+++ coffee-frontend/src/components/Content.js
@@ -10,17 +10,15 @@ const Content = ({
 }) => {
 
   return (
-    <div className='h-full'>
-      <div className='relative z-1 grid md:grid-cols-3 sm:grid-cols-1 gap-4'>
-        <Add 
-          openAddModal={openAddModal}
-        />
-        <CoffeeShops
-          listOfCoffeeShops={coffeeShops}
-          coffeeShopListFilter={filter}
-          onClickCoffeeShop={onClickCoffeeShop}
-        />
-      </div>
+    <div className='relative z-1 grid md:grid-cols-3 sm:grid-cols-1 gap-4'>
+      <Add 
+        openAddModal={openAddModal}
+      />
+      <CoffeeShops
+        listOfCoffeeShops={coffeeShops}
+        coffeeShopListFilter={filter}
+        onClickCoffeeShop={onClickCoffeeShop}
+      />
     </div>
   )
 }
