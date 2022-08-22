d1f9c0572292f729228de7ab2a6d14eb6fe96e0e
Giorgio <tran.giorgio@gmail.com>
2022-08-21 23:04:07 -1000

    Add responsiveness to header

--- coffee-frontend/src/components/Header.js
+++ coffee-frontend/src/components/Header.js
@@ -3,7 +3,7 @@ import Search from './Search'
 
 const Header = ({title, inputValue, onChangeFunction}) => {
   return (
-    <nav className='flex justify-between py-10'>
+    <nav className='flex gap-4 justify-between py-10 md:flex-row sm:flex-col xs:flex-col'>
       <h1 className='text-3xl font-bold text-darkbrown'> 
         {title} 
       </h1>
