274f07a0c023d9f2c494c30c177165a6cd4177c0
Giorgio <tran.giorgio@gmail.com>
2022-08-21 23:07:37 -1000

    Resize height of input box

--- coffee-frontend/src/components/Search.js
+++ coffee-frontend/src/components/Search.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Search = ({inputVal, onChangeFn}) => {
   return (
     <input
-      className='outline-none bg-beige box-border border-2 border-brown rounded-3xl w-80 px-5 placeholder-italic placeholder-darkbrown text-darkbrown' 
+      className='outline-none h-10 bg-beige box-border border-2 border-brown rounded-3xl w-80 px-5 placeholder-italic placeholder-darkbrown text-darkbrown' 
       value={inputVal} 
       placeholder='Search...'
       onChange={onChangeFn}
