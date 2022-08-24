1ff3b6b3cee80690c1c937bf1aa58ee97b8ecf54
Giorgio <tran.giorgio@gmail.com>
2022-08-23 23:18:30 -1000

    Edit spacing in Form

--- coffee-frontend/src/components/Form/Form.js
+++ coffee-frontend/src/components/Form/Form.js
@@ -3,16 +3,18 @@ import InputBox from './InputBox'
 
 const Form = ({ onSubmit, handleName, handleWebsite, handleLocation, handleDescription, close, enterName}) => {
   return (
-    <form className=''
+    <form className='flex flex-col h-full w-full justify-between'
       onSubmit={onSubmit}
     >
       <div className='flex justify-end'>
         <button className='' onClick={close} type='button'> x </button>
       </div>
-      <InputBox name='name' onChange={handleName} />
-      <InputBox name='website' onChange={handleWebsite} />
-      <InputBox name='location' onChange={handleLocation} />
-      <InputBox name='description' onChange={handleDescription} />
+      <div className='flex flex-col gap-10'>
+        <InputBox name='name' onChange={handleName} />
+        <InputBox name='website' onChange={handleWebsite} />
+        <InputBox name='location' onChange={handleLocation} />
+        <InputBox name='description' onChange={handleDescription} />
+      </div>
       <div className='flex justify-end gap-2 mb-2'>
         <button 
           className='border-box border-2 border-black' 
